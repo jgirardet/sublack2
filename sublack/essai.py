3fd9dcf77d138c49e601c788d38d29cf3977438d
jgirardet <ijkl@tuta.io>
2018-09-05 17:50:25 +0200

    m

---
+++ sublack/essai.py
@@ -0,0 +1,16 @@
+import subprocess as s
+
+# a = s.Popen(["blackd"])
+
+import requests
+import time
+
+# time.sleep(1)
+
+
+print(requests.post("http://localhost:45484"))
+
+# time.sleep(1)
+# time.sleep(10)
+
+# s.call(["taskkill", "/F", "/T", "/PID", str(a.pid)])
